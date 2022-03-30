cdd0dd7d6fdd830178e01fdd6af66aff83348103
Gibran Gonzalez <hector.gibran@hotmail.com>
2022-03-29 22:12:02 -0600

    Adds Authenticator UI component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <authenticator>
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </authenticator>
   </ion-app>
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonApp } from '@ionic/vue';
 import { defineComponent } from 'vue';
+
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
-    IonRouterOutlet
+    Authenticator
   }
 });
 </script>
