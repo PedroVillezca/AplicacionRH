f39585a0930a42ba5baa0c527a202cb073670ecd
Gibran Gonzalez <hector.gibran@hotmail.com>
2022-04-02 19:40:45 -0600

    Crea un componente independiente para el Authenticator

---
+++ src/components/AuthComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <authenticator 
+        :login-mechanisms="['username']"
+        :form-fields="formFields"
+        :sign-up-attributes="[
+        'username',
+        'given_name',
+        'family_name'
+        ]"
+        :sign-up-fields="[
+        {label: 'username', order: 4}
+        ]"
+    >
+        <app-header></app-header>
+        <ion-router-outlet />
+    </authenticator>
+</template>
+
+<script>
+import { I18n } from 'aws-amplify';
+import { Authenticator, translations } from "@aws-amplify/ui-vue";
+import { IonRouterOutlet } from '@ionic/vue';
+
+import "@aws-amplify/ui-vue/styles.css";
+import AppHeader from '../components/Header.vue';
+    
+export default {
+    name: 'AuthComponent',
+    components: {
+        Authenticator,
+        AppHeader,
+        IonRouterOutlet
+    }
+}
+
+I18n.putVocabularies(translations);
+I18n.setLanguage('es');
+I18n.putVocabularies({
+es:{
+    'Sign In': 'Iniciar Sesión',
+    'Create Account': 'Registrarse',
+    'Username': 'BlueTag',
+}
+});
+I18n.putVocabulariesForLanguage( 'es', {
+'Given Name': 'Nombre (s)',
+'Family Name': 'Apellidos',
+});
+</script>
