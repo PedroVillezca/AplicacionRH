77cf8c7c112459987c2d5531f624daacef8c9f88
Memo Saldana <memo.sal98@gmail.com>
2022-05-02 21:29:42 -0500

    removes alerts, keeps relevant logs

--- src/services/pushNotifications.js
+++ src/services/pushNotifications.js
@@ -27,21 +27,15 @@ const createEndpointForNewUser = async (token) => {
     }).promise()
     .then(data => resolve(data))
     .catch(err => {
-      alert('Error creating platform endpoint: ' + err);
-      alert(`${JSON.stringify(err)}`)
       if(err.code == 'InvalidParameter') {
-        alert(`INVALIDPARAMETER`)
         let matches = existingEndpointRegex.test(err.message);
         if(matches) {
-          alert('MATCHES')
           let arn = err.message.split(' ')[5];
           console.log(`ARN ${arn}`)
-          alert(`ARN ${arn}`)
           SNS.deleteEndpoint({
             EndpointArn: arn,
           }).promise()
             .then(() => {
-              alert(`DELETED PREVIOUS ENDPOINT`)
               return SNS.createPlatformEndpoint({
                 PlatformApplicationArn: String(platformApplicationArn),
                 Token: token.value,
@@ -63,11 +57,8 @@ const notificationInitialSetup = () => {
   // On success, we should be able to receive notifications
   PushNotifications.addListener('registration',
     (token) => {
-      alert('registration succeeded! ' + token.value)
       createEndpointForNewUser(token)
         .then(data => {
-          alert('Push registration success, token: ' + token.value + '\n data: ' + data.EndpointArn);
-          alert(`ENDPOINT ${JSON.stringify(data)}`)
           endpoint = data.EndpointArn
           return Storage.set({
             key: 'endpoint',
@@ -75,14 +66,13 @@ const notificationInitialSetup = () => {
           })
         })
         .then(() => {
-          alert(`endpoint ${endpoint}`)
           return subscribeEmployee(endpoint)
         })
         .then(subArn => {
-          alert('Subscribed successfully to employee topic: ' + subArn)
+          console.log('Subscribed successfully to employee topic: ' + subArn)
         })
         .catch(err => {
-          alert('Error on initial notifications setup: ' + err);
+          console.log('Error on initial notifications setup: ' + err);
         })
     }
   )
@@ -90,7 +80,7 @@ const notificationInitialSetup = () => {
   // Some issue with our setup and push will not work
   PushNotifications.addListener('registrationError',
     (error) => {
-      alert('Error on registration: ' + JSON.stringify(error));
+      console.log('Error on registration: ' + JSON.stringify(error));
     }
   );
 
@@ -104,7 +94,7 @@ const notificationInitialSetup = () => {
   // Method called when tapping on a notification
   PushNotifications.addListener('pushNotificationActionPerformed',
     (notification) => {
-      alert('Push action performed: ' + JSON.stringify(notification));
+      console.log('Push action performed: ' + JSON.stringify(notification));
     }
   );
 }
@@ -113,7 +103,6 @@ const setupAuthListeners = () => {
   console.log('adding listeners')
   Hub.listen('auth', (data) => {
     console.log(`Auth event ${JSON.stringify(data)}`)
-    alert(`Auth event ${JSON.stringify(data)}`);
     switch(data.payload.event) {
       case 'signIn':
         console.log('signed in');
@@ -142,14 +131,14 @@ const setupAuthListeners = () => {
           }).promise()
         })
         .then((data) => {
-          alert('endpoint disabled', data)
+          console.log('endpoint disabled', data)
           return Storage.remove({key: 'endpoint'})
         })
         .then(() => {
-          alert('endpoint removed from storage')
+          console.log('endpoint removed from storage')
         })
         .catch(err => {
-          alert(`error setting endpoint attributes ${JSON.stringify(err)}`)
+          console.log(`error setting endpoint attributes ${JSON.stringify(err)}`)
         })
 
         break;
@@ -159,7 +148,7 @@ const setupAuthListeners = () => {
 
 export const subscribeEmployee = (endpointArn) => {
   return new Promise((res, rej) => {
-    alert(`SUB ARN ${endpointArn}`)
+    console.log(`SUB ARN ${endpointArn}`)
     SNS.subscribe({
       Protocol: 'application',
       TopicArn: topicArn,
