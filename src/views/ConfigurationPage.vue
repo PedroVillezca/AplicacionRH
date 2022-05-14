5b769ff4bad0ddeb5a28b50028c2ba644e9b43b2
Memo Saldana <memo.sal98@gmail.com>
2022-05-14 18:54:51 -0500

    checks if date numbers are NaN and uses toast to show error

--- src/views/ConfigurationPage.vue
+++ src/views/ConfigurationPage.vue
@@ -83,8 +83,14 @@ export default defineComponent({
       var mes = parseInt(this.month);
       var anio = parseInt(this.year);
 
-      if (dia > 31 || dia < 1 || mes < 1 || mes > 12 || anio < 1950){
-        alert("Verificar la fecha")
+      if ( isNaN(dia) || isNaN(mes) || isNaN(anio) ||
+           dia > 31 || dia < 1 || mes < 1 || mes > 12 || anio < 1950){
+        const toast = await toastController.create({
+          message:"Verificar la fecha",
+          color: 'danger',
+          duration: 2000,
+          })
+        toast.present()
       } else{
         const updatedUser = {
           blueTag: this.blueTag,
