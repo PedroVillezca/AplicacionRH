bd726d08c06831ea19d95a0bb64049ad0ae950cc
Gibran Gonzalez <hector.gibran@hotmail.com>
2022-05-21 15:48:15 -0500

    Creates notification item component, doesnt show

---
+++ src/components/NotificationItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>template</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'NotificationItem',
+  components: {
+
+  },
+  data() {
+    return {
+      blueTag: "",
+      date: "",
+      message: ""
+    }
+  },
+  props: ["notificationData"],
+  created() {
+    console.log("I CAME IN")
+    this.getNotificationData()
+  },
+  methods: {
+    getNotificationData() {
+      console.log(this.notificationData)
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,8 +1,18 @@
 <template>
   <ion-page>
-    <ion-content>
-      <div>
-        <ion-text>Este va a ser el historial de notificaciones, el arreglo en 'data' tiene los objetos de notificaciones de Dynamo.</ion-text>
+    <ion-content :scroll-events="true">
+      <div id="container">
+        <div id="conf-title">
+          <ion-button @click="$router.go(-1)">
+            <ion-icon name="arrow-back"></ion-icon>
+          </ion-button>
+          Historial
+        </div>
+
+        <p>TEEEEEXTTT</p>
+        <div v-bind:key="notification.blueTag" v-for="notification in notifications">
+          <notification-item v-bind:notificationData="notification" />
+        </div>
       </div>
     </ion-content>
   </ion-page>
@@ -10,16 +20,17 @@
 
 <script>
 import { Auth, API } from 'aws-amplify';
-import { IonContent, IonPage, IonText  } from '@ionic/vue';
+import { IonContent, IonPage } from '@ionic/vue';
 import { getUser } from '../graphql/queries'
 import { listNotifications } from '../graphql/queries'
+import { NotificationItem } from '../components/NotificationItem.vue'
 
 export default {
   name: 'NotificationPage',
   components: {
     IonContent,
     IonPage,
-    IonText
+    NotificationItem
   },
   data() {
     return {
@@ -39,13 +50,47 @@ export default {
     }
   },
   async created() {
+    console.log("ENTER CREATED")
     var userDynamo = await this.getDynamoUser()
+    console.log("AFTER DYNAMO USER")
     this.blueTag = userDynamo.blueTag
     this.notifications = await this.getNotifications(this.blueTag)
+    console.log("AFTER NOTIFICATIONS")
+    console.log(this.notifications)
   }
 }
 </script>
 
 <style>
+#conf-title {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  margin-top: 10px;
+  font-size: 50px;
+}
 
+#container {
+  position: absolute;
+  left: 20px;
+  right: 0;
+  top: 20%;
+  /* transform: translateY(-50%); */
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
 </style>
